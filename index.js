c6d104505955df6a5c3391ab5b6ef104c42670a1
Chaitanya82 <chaitanyamohansharma82@gmail.com>
2025-10-06 11:06:44 +0530

    3rd Commit 06102025

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+    const path = require('path');
+    const app = express();
+    const port = 3000;
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
+    // Serve static files from a 'public' directory (where your HTML and other assets are)
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    // Define a route to serve your HTML file
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
